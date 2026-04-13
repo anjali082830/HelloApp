306c3a26337972b29197c36f8ab16ec7b023b1ad
anjali082830 <anjalikumaridhanbad16@gmail.com>
2026-04-13 15:07:00 +0530

    Added UC3

--- HelloApp.java
+++ HelloApp.java
@@ -1,12 +1,15 @@
 public class HelloApp {
     public static void main(String[] args) {
-		String name="World";
-        if (args.length > 0) {
-            name = args[0];
-            System.out.println("Hello, "+name+"!");
+        String name="World";
+        if (args.length>0) {
+            StringBuilder nameBuilder= new StringBuilder();
+            for (String n:args) {
+                nameBuilder.append(n).append(", ");
+            }
+            if (nameBuilder.length()>0) {
+                name = nameBuilder.substring(0, nameBuilder.length() - 2);
+            }
         }
-		else {
-			System.out.println("Hello, "+name+"!");
-		}
+        System.out.println("Hello, "+name+"!");
     }
 }
