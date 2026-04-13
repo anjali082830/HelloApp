2ada1796531d4007a454b0efac1d28272c2283f5
anjali082830 <anjalikumaridhanbad16@gmail.com>
2026-04-13 15:50:48 +0530

    Added UC5

--- HelloApp.java
+++ HelloApp.java
@@ -1,10 +1,18 @@
-public class HelloApp {
-    public static void main(String[] args) {
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, "+name+"!");
-        } else {
-            System.out.println("Hello, World!");
+public class HelloApp{
+    public static void main(String[] args){
+        String name = "World";
+        if (args.length>0){
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
+            for (String n : args){
+                if (!first) {
+                    nameBuilder.append(", ");
+                }
+                nameBuilder.append(n);
+                first = false;
+            }
+            name = nameBuilder.toString();
         }
+        System.out.println("Hello, "+name+"!");
     }
 }
